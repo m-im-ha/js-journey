5a90cca85030c7aad07849d198918a120ad94ed7
Imran <sherlockholmesad221@gmail.com>
2024-01-25 13:04:17 +0600

    initial commit

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -1166,12 +1166,16 @@ whereAmI(-33.933, 18.474);
 //////////////////////////////////////////////
 //////////////////////////////////////
 
-async function pokemon_data(pokemon) {
-  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-  console.log(response);
-  let data = await response.json().then(data=>
-    {console.log(data);
-    console.log(data.sprites.front_shiny)});
-  return data;
-}
-pokemon_data("ho-oh");
+// async function pokemon_data(pokemon) {
+//   let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+//   console.log(response);
+//   let data = await response.json().then(data=>
+//     {
+//     console.log(data);
+//     console.log(data.sprites.front_shiny);
+//     console.log(data.sprites.back_default);
+//     console.log(data.sprites.other["dream_world"]["front_default"]);
+//   });
+//   return data;
+// }
+// pokemon_data("pikachu");
